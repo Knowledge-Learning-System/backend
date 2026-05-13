fda49f65f65bdc6f6692a0511bd862a98bf42f37
wang-jingnan-n <2068623628@qq.com>
2026-05-13 11:05:42 +0800

    添加完整项目代码：pom.xml 和 src 目录

---
+++ src/test/java/cn/edu/bcu/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bcu.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
